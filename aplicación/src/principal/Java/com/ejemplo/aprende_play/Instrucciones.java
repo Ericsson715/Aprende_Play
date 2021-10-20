1199b984eb42eb2a430b49d7f596138de3c42da4
Ericsson715 <92638586+Ericsson715@users.noreply.github.com>
2021-10-20 17:00:21 -0500

    Update and rename app/src/main/java/com/example/aprende_play/Instrucciones.java to aplicación/src/principal/Java/com/ejemplo/aprende_play/Instrucciones.java
    Cambio de prueba

--- aplicación/src/principal/Java/com/ejemplo/aprende_play/Instrucciones.java
+++ aplicación/src/principal/Java/com/ejemplo/aprende_play/Instrucciones.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class Instrucciones extends AppCompatActivity {
-
+//Mariano y axol
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
